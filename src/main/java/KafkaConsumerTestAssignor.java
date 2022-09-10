d8868d05947c8688724119222cf17da61584509c
m.ezzeddine <mazen.ezzeddine@gmail.com>
2022-09-10 10:53:38 +0200

    heteregenous consumers working

---
+++ src/main/java/KafkaConsumerTestAssignor.java
@@ -0,0 +1,17 @@
+
+
+public class KafkaConsumerTestAssignor {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread consumer = new Thread  (new ConsumerThread());
+        Thread server = new Thread  (new ServerThread());
+        consumer.start();
+        server.start();
+        consumer.join();
+
+    }
+}
+
+
+
+
